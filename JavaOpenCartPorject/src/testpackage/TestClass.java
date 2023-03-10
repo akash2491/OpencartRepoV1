8b8f2a1fec9163258778775057e61f5244bfc6b2
akash2491 <127483979+akash2491@users.noreply.github.com>
2023-03-10 15:11:38 +0530

    First commit

---
+++ JavaOpenCartPorject/src/testpackage/TestClass.java
@@ -0,0 +1,13 @@
+package testpackage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("TestBase Class");
+		System.out.println("pom.xml");
+		System.out.println("Util");
+
+	}
+
+}
